f76e34e569a50bd3aae52cdcb77abcfa91955b37
insulatoreater69420 <94189775+insulatoreater69420@users.noreply.github.com>
2021-12-03 11:36:26 -0500

    Update IodineGTACoreGlueCode.js

--- user_scripts/IodineGTACoreGlueCode.js
+++ user_scripts/IodineGTACoreGlueCode.js
@@ -48,6 +48,7 @@ var games = {
     "fzero_max":"F-Zero - Maximum Velocity",
     "gamewatch4":"Game & Watch Gallery 4",
     "goldensun":"Golden Sun",
+    "grandtheftadvance":"Grand Theft Auto Advance",
     "gunstar_super_heroes":"Gunstar Super Heroes",
     "hamtaro_heartbreak":"Hamtaro - Ham-Ham Heartbreak",
     "kirbymirror":"Kirby & The Amazing Mirror",
