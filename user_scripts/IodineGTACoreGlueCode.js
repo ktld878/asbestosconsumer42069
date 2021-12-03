bbf987412666551745379442cc4f68ae7cafbe9e
insulatoreater69420 <94189775+insulatoreater69420@users.noreply.github.com>
2021-12-03 16:15:12 -0500

    Update IodineGTACoreGlueCode.js

--- user_scripts/IodineGTACoreGlueCode.js
+++ user_scripts/IodineGTACoreGlueCode.js
@@ -84,6 +84,7 @@ var games = {
     "pacman_world2":"Pacman World 2",
     "pokemoncollection":"The Pokemon Gameboy Collection",
     "darkrising":"Pokemon: Dark Rising 1 (Fire Red Hack)",
+    "darkrising2":"Pokemon: Dark Rising 2 (Fire Red Hack)",
     "pokemonflorasky":"Pokemon Flora Sky Rom Hack",
     "pokemonemerald":"Pokemon Emerald",
     "eprp":"Pokemon Emerald Party Randomizer Plus",
