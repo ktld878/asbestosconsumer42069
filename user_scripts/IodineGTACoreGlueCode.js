118763ff680a9f452c5f4152fdf0d9549f95b1c9
insulatoreater69420 <94189775+insulatoreater69420@users.noreply.github.com>
2021-12-03 08:22:21 -0500

    Update IodineGTACoreGlueCode.js

--- user_scripts/IodineGTACoreGlueCode.js
+++ user_scripts/IodineGTACoreGlueCode.js
@@ -33,7 +33,7 @@ var games = {
     "dbz_supersonic":"Dragon Ball Z - Supersonic Warriors",
     "drilldozer":"Drill Dozer",
     "dukenukem":"Duke Nukem Advance",
-    "dungeonsanddragons":"Dungeons and Dragons - Eye of the Beholder",
+    "dnd":"Dungeons and Dragons - Eye of The Beholder",
     "earthwormjim":"Earthworm Jim",
     "earthwormjim2":"Earthworm Jim 2",
     "ff1and2":"Final Fantasy 1 & 2 Advance",
